e48ef5f30eea1d2505f8484047c60565b3606487
Cheng Sun <s2821d3721@gmail.com>
2018-08-10 15:30:25 +0800

    Implement left-right flip in visualization

--- visual.py
+++ visual.py
@@ -26,6 +26,10 @@ parser.add_argument('--line_glob', required=True,
                          'two list with same index are load as input channels'
                          'Note: Remeber to quote your glob path')
 parser.add_argument('--output_dir', required=True)
+# Data augmented arguments (to improve output quality)
+parser.add_argument('--flip', action='store_true',
+                    help='Whether to perfome left-right flip'
+                         '# of input x2')
 # Visualization related arguments
 parser.add_argument('--alpha', default=0.8,
                     help='Weight to composite output with origin rgb image')
@@ -47,6 +51,28 @@ img_paths = sorted(glob.glob(args.img_glob))
 line_paths = sorted(glob.glob(args.line_glob))
 assert len(img_paths) == len(line_paths), '# of input mismatch for each channels'
 
+
+def augment(x_img):
+    aug_type = ['']
+    x_imgs_augmented = [x_img]
+    if args.flip:
+        aug_type.append('flip')
+        x_imgs_augmented.append(np.flip(x_img, axis=-1))
+    return np.array(x_imgs_augmented), aug_type
+
+
+def augment_undo(x_imgs_augmented, aug_type):
+    x_imgs = []
+    for x_img, aug in zip(x_imgs_augmented, aug_type):
+        if aug == 'flip':
+            x_imgs.append(np.flip(x_img, axis=-1))
+        elif aug == '':
+            x_imgs.append(x_img)
+        else:
+            raise NotImplementedError()
+    return np.array(x_imgs)
+
+
 # Process each input
 for i_path, l_path in zip(img_paths, line_paths):
     print('img  path:', i_path)
@@ -59,21 +85,30 @@ for i_path, l_path in zip(img_paths, line_paths):
         i_img.transpose([2, 0, 1]),
         l_img.transpose([2, 0, 1])], axis=0)
 
+    # Augment data
+    x_imgs_augmented, aug_type = augment(x_img)
+
     # Feedforward and extract output images
     with torch.no_grad():
-        x = torch.FloatTensor(x_img).unsqueeze(0).to(device)
+        x = torch.FloatTensor(x_imgs_augmented).to(device)
         en_list = encoder(x)
         edg_de_list = edg_decoder(en_list[::-1])
         cor_de_list = cor_decoder(en_list[-1:] + edg_de_list[:-1])
 
-        edg_tensor = torch.sigmoid(edg_de_list[-1].squeeze())
-        cor_tensor = torch.sigmoid(cor_de_list[-1].squeeze())
+        edg_tensor = torch.sigmoid(edg_de_list[-1])
+        cor_tensor = torch.sigmoid(cor_de_list[-1])
 
-        edg_img = edg_tensor.cpu().numpy().transpose([1, 2, 0])
-        cor_img = cor_tensor.cpu().numpy()
+        # Recover the effect from augmentation
+        edg_img = augment_undo(edg_tensor.cpu().numpy(), aug_type)
+        cor_img = augment_undo(cor_tensor.cpu().numpy(), aug_type)
 
+        # Merge all results from augmentation
+        edg_img = edg_img.transpose([0, 2, 3, 1]).mean(0)
+        cor_img = cor_img.transpose([0, 2, 3, 1]).mean(0)
+
+    # Composite output image with rgb image
     edg_img = args.alpha * edg_img + (1 - args.alpha) * i_img
-    cor_img = args.alpha * cor_img[..., None] + (1 - args.alpha) * i_img
+    cor_img = args.alpha * cor_img + (1 - args.alpha) * i_img
 
     # Dump result
     basename = os.path.splitext(os.path.basename(i_path))[0]
