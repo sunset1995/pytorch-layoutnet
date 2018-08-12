b7ddba9014fbbbd3132fa09850fe91c741adc009
Cheng Sun <s2821d3721@gmail.com>
2018-08-12 12:42:48 +0800

    Convert label corner as well

--- torch2pytorch_data.py
+++ torch2pytorch_data.py
@@ -1,6 +1,8 @@
 import os
+import glob
 import torchfile
 import numpy as np
+from scipy.io import loadmat
 
 from PIL import Image
 
@@ -63,3 +65,60 @@ def cvt2png(target_dir, patterns):
 cvt2png(os.path.join(DATA_DIR, 'train'), train_pats)
 cvt2png(os.path.join(DATA_DIR, 'valid'), valid_pats)
 cvt2png(os.path.join(DATA_DIR, 'test'), test_pats)
+
+# Copy ground truth corner label
+train_set = set(os.listdir(os.path.join(DATA_DIR, 'train', cat_list[0])))
+valid_set = set(os.listdir(os.path.join(DATA_DIR, 'valid', cat_list[0])))
+test_set = set(os.listdir(os.path.join(DATA_DIR, 'test', cat_list[0])))
+
+train_set = set([v[:-4] for v in train_set])
+valid_set = set([v[:-4] for v in valid_set])
+test_set = set([v[:-4] for v in test_set])
+assert len(train_set.intersection(valid_set)) == 0, 'data split overlapped'
+assert len(train_set.intersection(test_set)) == 0, 'data split overlapped'
+assert len(valid_set.intersection(test_set)) == 0, 'data split overlapped'
+
+os.makedirs(os.path.join(DATA_DIR, 'train', 'label_cor'), exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'valid', 'label_cor'), exist_ok=True)
+os.makedirs(os.path.join(DATA_DIR, 'test', 'label_cor'), exist_ok=True)
+
+problems = {
+    'no match image': [],
+    'no match labeled corner': {
+        'train': set(train_set),
+        'valid': set(valid_set),
+        'test': set(test_set),
+    },
+}
+for path in glob.glob(os.path.join(ORGIN_GT_DIR, 'label_cor', '**', '*')):
+    k = os.path.basename(path)[:-4]
+    mat = loadmat(path)['cor'][:, :2]
+    assert mat.shape[0] == 8 or mat.shape[0] == 12
+    if k in train_set:
+        problems['no match labeled corner']['train'].remove(k)
+        if mat.shape[0] == 8:
+            np.save(os.path.join(DATA_DIR, 'train', 'label_cor', k),
+                    mat)
+    elif k in valid_set:
+        problems['no match labeled corner']['valid'].remove(k)
+        if mat.shape[0] == 8:
+            np.save(os.path.join(DATA_DIR, 'valid', 'label_cor', k),
+                    mat)
+    elif k in test_set:
+        problems['no match labeled corner']['test'].remove(k)
+        if mat.shape[0] == 8:
+            np.save(os.path.join(DATA_DIR, 'test', 'label_cor', k),
+                    mat)
+    else:
+        problems['no match image'].append(path)
+
+if len(problems['no match image']):
+    print('\nno match image:')
+    for v in problems['no match image']:
+        print(v)
+
+for k, st in problems['no match labeled corner'].items():
+    if len(st):
+        print('\nno match labeled corner (%s)' % k)
+        for v in st:
+            print(v)
