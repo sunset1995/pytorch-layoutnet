916227507de63d56dfa116769187d068d086e959
Cheng Sun <s2821d3721@gmail.com>
2019-01-05 15:34:23 +0800

    Refactor eval script

--- eval.py
+++ eval.py
@@ -1,15 +1,15 @@
 import os
 import argparse
 import numpy as np
-from itertools import chain
 
 import torch
-import torch.nn as nn
-from torch import optim
 from torch.utils.data import DataLoader
 from model import Encoder, Decoder
 from dataset import PanoDataset
 from utils import StatisticDict
+from pano import get_ini_cor
+from pano_opt import optimize_cor_id
+from utils_eval import eval_PE, eval_3diou, augment, augment_undo
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -25,10 +25,20 @@ parser.add_argument('--input_cat', default=['img', 'line'], nargs='+',
                     help='input channels subdirectories')
 parser.add_argument('--input_channels', default=6, type=int,
                     help='numbers of input channels')
-parser.add_argument('--num_workers', default=6, type=int,
-                    help='numbers of workers for dataloaders')
-parser.add_argument('--batch_size', default=4, type=int,
-                    help='mini-batch size')
+parser.add_argument('--d1', default=21, type=int,
+                    help='Post-processing parameter.')
+parser.add_argument('--d2', default=3, type=int,
+                    help='Post-processing parameter.')
+# Augmentation related
+parser.add_argument('--flip', action='store_true',
+                    help='whether to perfome left-right flip. '
+                         '# of input x2.')
+parser.add_argument('--rotate', nargs='*', default=[], type=float,
+                    help='whether to perfome horizontal rotate. '
+                         'each elements indicate fraction of image width. '
+                         '# of input xlen(rotate).')
+parser.add_argument('--post_optimization', action='store_true',
+                    help='whether to performe post gd optimization')
 args = parser.parse_args()
 device = torch.device(args.device)
 
@@ -37,11 +47,8 @@ device = torch.device(args.device)
 dataset = PanoDataset(root_dir=args.root_dir,
                       cat_list=[*args.input_cat, 'edge', 'cor'],
                       flip=False, rotate=False,
-                      gamma=False)
-loader = DataLoader(dataset, args.batch_size,
-                    shuffle=False, drop_last=False,
-                    num_workers=args.num_workers,
-                    pin_memory=args.device.startswith('cuda'))
+                      gamma=False,
+                      return_filenames=True)
 
 
 # Prepare model
@@ -54,34 +61,63 @@ cor_decoder.load_state_dict(torch.load('%s_cor_decoder.pth' % args.path_prefix))
 
 
 # Start evaluation
-criti = nn.BCEWithLogitsLoss(reduction='none')
 test_losses = StatisticDict()
-for ith, datas in enumerate(loader):
-    print('processed %d batches out of %d' % (ith, len(loader)), end='\r', flush=True)
-    with torch.no_grad():
-        # Prepare data
-        x = torch.cat([datas[i]
-                      for i in range(len(args.input_cat))], dim=1).to(device)
-        y_edg = datas[-2].to(device)
-        y_cor = datas[-1].to(device)
-        b_sz = x.size(0)
+test_pano_losses = StatisticDict()
+test_2d3d_losses = StatisticDict()
+for ith, datas in enumerate(dataset):
+    print('processed %d batches out of %d' % (ith, len(dataset)), end='\r', flush=True)
 
-        # Feedforward
-        en_list = encoder(x)
+    x = torch.cat([datas[i] for i in range(len(args.input_cat))], dim=0).numpy()
+    x_augmented, aug_type = augment(x, args.flip, args.rotate)
+
+    with torch.no_grad():
+        x_augmented = torch.FloatTensor(x_augmented).to(device)
+        en_list = encoder(x_augmented)
         edg_de_list = edg_decoder(en_list[::-1])
         cor_de_list = cor_decoder(en_list[-1:] + edg_de_list[:-1])
-        y_edg_ = edg_de_list[-1]
-        y_cor_ = cor_de_list[-1]
+        edg_tensor = torch.sigmoid(edg_de_list[-1])
+        cor_tensor = torch.sigmoid(cor_de_list[-1])
 
-        # Compute training objective loss
-        loss_edg = criti(y_edg_, y_edg)
-        loss_edg[y_edg == 0.] *= 0.2
-        loss_edg = loss_edg.mean().item()
-        loss_cor = criti(y_cor_, y_cor)
-        loss_cor[y_cor == 0.] *= 0.2
-        loss_cor = loss_cor.mean().item()
+        # Recover the effect from augmentation
+        edg_img = augment_undo(edg_tensor.cpu().numpy(), aug_type)
+        cor_img = augment_undo(cor_tensor.cpu().numpy(), aug_type)
 
-    test_losses.update('edg loss', loss_edg, weight=b_sz)
-    test_losses.update('cor loss', loss_cor, weight=b_sz)
+        # Merge all results from augmentation
+        edg_img = edg_img.transpose([0, 2, 3, 1]).mean(0)
+        cor_img = cor_img.transpose([0, 2, 3, 1]).mean(0)[..., 0]
 
-print('[RESULT] %s' % (test_losses), flush=True)
+    # Load ground truth corner label
+    k = datas[-1][:-4]
+    path = os.path.join(args.root_dir, 'label_cor', '%s.txt' % k)
+    with open(path) as f:
+        gt = np.array([line.strip().split() for line in f], np.float64)
+
+    # Construct corner label from predicted corner map
+    cor_id = get_ini_cor(cor_img, args.d1, args.d2)
+
+    # Gradient descent optimization
+    if args.post_optimization:
+        cor_id = optimize_cor_id(cor_id, edg_img, cor_img,
+                                 num_iters=100, verbose=False)
+
+    # Compute normalized corner error
+    cor_error = ((gt - cor_id) ** 2).sum(1) ** 0.5
+    cor_error /= np.sqrt(cor_img.shape[0] ** 2 + cor_img.shape[1] ** 2)
+    pe_error = eval_PE(cor_id[0::2], cor_id[1::2], gt[0::2], gt[1::2])
+    iou3d = eval_3diou(cor_id[1::2], cor_id[0::2], gt[1::2], gt[0::2])
+    test_losses.update('CE(%)', cor_error.mean() * 100)
+    test_losses.update('PE(%)', pe_error * 100)
+    test_losses.update('3DIoU', iou3d)
+
+    if k.startswith('pano'):
+        test_pano_losses.update('CE(%)', cor_error.mean() * 100)
+        test_pano_losses.update('PE(%)', pe_error * 100)
+        test_pano_losses.update('3DIoU', iou3d)
+    else:
+        test_2d3d_losses.update('CE(%)', cor_error.mean() * 100)
+        test_2d3d_losses.update('PE(%)', pe_error * 100)
+        test_2d3d_losses.update('3DIoU', iou3d)
+
+print('[RESULT overall     ] %s' % (test_losses), flush=True)
+print('[RESULT panocontext ] %s' % (test_pano_losses), flush=True)
+print('[RESULT stanford2d3d] %s' % (test_2d3d_losses), flush=True)
